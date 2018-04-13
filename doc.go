d6e024e58aea2d88a930461a6a4c304ef329aadb
Ricardo Cámara <rcamara9@gmail.com>
2018-04-13 16:52:37 +0200

    added doc

---
+++ doc.go
@@ -0,0 +1,8 @@
+// This tiny script starts at version 0.0.1
+// Now you can run the script only if you had go in your computer
+// newer version won't need that and as you see we are in the main package
+// and not in a custom package
+
+// Usage example go run convert_log_to_csv/*.go -file={fullinputfilepath} -sep={stringseparator} -outfile={fulloutputfilepath}
+
+package main
