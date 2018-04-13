838cd8e006bf6cd43a2e4058a5ba416ad3566427
Ricardo Cámara <rcamara9@gmail.com>
2018-04-13 19:54:54 +0200

    Improved doc

--- doc.go
+++ doc.go
@@ -3,7 +3,7 @@
 // You have to ways:
 //    1. Usage example go run convert_log_to_csv/*.go -file={fullinputfilepath} -sep={stringseparator} -outfile={fulloutputfilepath}
 //	  2. Making use of the Makefile. With simply typing make release inside the project directory
-//       you will get the binaries for the most used operating systems
-
+//       you will get the binaries for the most used operating systems and then execute it doing:
+//       convert_log_to_csv-{VERSION}-{ARCH} -file={fullinputfilepath} -sep={stringseparator} -outfile={fulloutputfilepath}
 
 package main
